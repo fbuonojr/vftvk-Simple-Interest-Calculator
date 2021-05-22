18788018d702627ff39fe8e0e7db837ddf479d42
Frank Buono <fbuonojr@fordham.edu>
2021-05-21 22:48:14 -0400

    compute function

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate / 100;
 var year = new Date().getFullYear() + parseInt(years);
-var result = document.getElementById("result");
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
@@ -11,6 +10,8 @@ function updateRate() {
 }
 
 function compute() {
-    // p = document.getElementById("principal").value;
-
+    document.getElementById("result").innerHTML="If you deposit "+principal+
+    ",\<br\>at an interest rate of "+rate+
+    "%\<br\>You will receive an amount of "+interest+
+    ",\<br\>in the year "+year+"\<br\>";
 }
