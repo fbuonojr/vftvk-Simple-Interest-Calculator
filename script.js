f8d9bb043d992f3476b65ee6a23d0c636674f6a3
Frank Buono <fbuonojr@fordham.edu>
2021-05-21 22:35:00 -0400

    added result var

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate / 100;
 var year = new Date().getFullYear() + parseInt(years);
+var result = document.getElementById("result").value;
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
