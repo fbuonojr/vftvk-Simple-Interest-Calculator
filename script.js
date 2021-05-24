6c9b65c0c8fe68543107b21de15d11e385a1facb
Frank Buono <fbuonojr@fordham.edu>
2021-05-24 12:55:07 -0400

    moved variable declaration

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate / 100;
-var year = new Date().getFullYear() + parseInt(years);
+// var principal = document.getElementById("principal").value;
+// var rate = document.getElementById("rate").value;
+// var years = document.getElementById("years").value;
+// var interest = principal * years * rate / 100;
+// var year = new Date().getFullYear() + parseInt(years);
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
@@ -10,6 +10,13 @@ function updateRate() {
 }
 
 function compute() {
+
+    var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate / 100;
+var year = new Date().getFullYear() + parseInt(years);
+
     document.getElementById("result").innerHTML="If you deposit "+principal+
     ",\<br\>at an interest rate of "+rate+
     "%\<br\>You will receive an amount of "+interest+
