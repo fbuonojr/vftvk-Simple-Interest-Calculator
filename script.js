6098c7ce61c82bccfd20ddcf0152b9521530ba1e
Frank Buono <fbuonojr@fordham.edu>
2021-05-24 15:23:20 -0400

    validator for principal

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// var principal = document.getElementById("principal").value;
-// var rate = document.getElementById("rate").value;
-// var years = document.getElementById("years").value;
-// var interest = principal * years * rate / 100;
-// var year = new Date().getFullYear() + parseInt(years);
+
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
@@ -12,13 +8,19 @@ function updateRate() {
 function compute() {
 
     var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate / 100;
-var year = new Date().getFullYear() + parseInt(years);
-
-    document.getElementById("result").innerHTML="If you deposit "+principal+
-    ",\<br\>at an interest rate of "+rate+
-    "%\<br\>You will receive an amount of "+interest+
-    ",\<br\>in the year "+year+"\<br\>";
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    if (principal <= 0) {
+        document.getElementById("result").innerHTML = "Principal must be positive";
+        principal.focus();
+        principal.scrollIntoView();
+    }
+    else {
+        document.getElementById("result").innerHTML = "If you deposit " + principal +
+            ",\<br\>at an interest rate of " + rate +
+            "%\<br\>You will receive an amount of " + interest +
+            ",\<br\>in the year " + year + "\<br\>";
+    }
 }
